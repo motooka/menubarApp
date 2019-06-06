d6ad040892ebe88d31939d0621e1844f2f0fe851
Tadahisa MOTOOKA <motooka@jotojoho.co.jp>
2019-06-07 00:20:46 +0900

    created empty cocoa mac app

---
+++ menubarApp/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  menubarApp
+//
+//  Created by T. MOTOOKA on 2019/06/07.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ menubarApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  menubarApp
+//
+//  Created by T. MOTOOKA on 2019/06/07.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
